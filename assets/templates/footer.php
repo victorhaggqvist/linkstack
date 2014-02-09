95ec7609e0f0e278a73960f0c0d22120c23b6e0b
Victor Häggqvist <victor@snilius.com>
2014-02-09 16:17:32 +0100

    .

--- assets/templates/footer.php
+++ assets/templates/footer.php
@@ -13,7 +13,7 @@
       </div>
     </div>
   </footer>
-</div> <!-- /container -->
+</div> <!-- /.container -->
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
