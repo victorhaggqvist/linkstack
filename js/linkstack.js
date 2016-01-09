524ef50dd970ddfd7a7d38bcd6bbb122acc111a0
Victor Häggqvist <victor@snilius.com>
2016-01-09 19:20:14 +0100

    tweak js

--- js/linkstack.js
+++ js/linkstack.js
@@ -5,7 +5,6 @@
 (function () {
     'use strict';
 
-    var _form = document.querySelector('#pushform');
     var _url = document.querySelector('#form_url');
     var _title = document.querySelector('#form_title');
     var _tags = document.querySelector('#form_tags');
@@ -18,7 +17,7 @@
     var METASERVICE = 'https://tool.stack.snilius.com';
     var _fetchmutex = false;
 
-    var _loadinfo = function () {
+    var _loadinfo = () => {
         if (_fetchmutex) return;
 
         if (_url.value.length < 5) return;
@@ -28,25 +27,24 @@
         _fetchmutex = true;
         var apiUrl = METASERVICE + '/info?url=' + _url.value;
         fetch(apiUrl)
-        .then(resp => resp.json(), err => console.error('meta fetch fail: ' + err))
-        .then(ret => {
-            if (ret === undefined) return;
-            _fetchmutex = false;
-            if (_title.value.length < 1) {
-                _title.value = ret.title;
-            }
+            .then(resp => resp.json(),
+                err => {
+                    _fetchmutex = false;
+                    console.error('meta fetch fail: ' + err)
+                })
+            .then(ret => {
+                if (ret === undefined) return;
+                _fetchmutex = false;
 
-            if (_tags.value.length < 1) {
-                _tags.value = ret.meta;
-            }
+                if (_title.value.length < 1) _title.value = ret.title;
 
-            if (_url.value !== ret.longurl) {
-                _url.value = ret.longurl;
-            }
-        });
+                if (_tags.value.length < 1) _tags.value = ret.meta;
+
+                if (_url.value !== ret.longurl) _url.value = ret.longurl;
+            });
     };
 
-    var _deleteItem = function(){
+    var _deleteItem = () => {
         console.log(this);
         var tempItem = this;
         var id = this.getAttribute('data-id');
@@ -55,12 +53,11 @@
         fetch('/api/items/' + id, {
             method: 'delete',
             credentials: 'include'
-        }).then((resp) => {
-            if (resp.status === 204) {
-                tempItem.parentNode.parentNode.remove();
-            }
+        }).then(resp => {
+            if (resp.status === 204) tempItem.parentNode.parentNode.remove();
+
             console.log(resp);
-        }).catch(err => console.error(err));
+        }, err => console.error(err));
     };
 
     var _putRecent = function(url, title, tags, id) {
@@ -91,7 +88,7 @@
         binder.onclick = _deleteItem;
     };
 
-    _save.onclick = function (event) {
+    _save.onclick = event => {
         event.preventDefault();
 
         var body = {
@@ -105,33 +102,33 @@
             method: 'post',
             body: JSON.stringify(body),
             credentials: 'include'
-        }).then(resp => {
-            return resp.json();
-        }).then(json => {
-            if ('message' in json) {
-                _statusMsg.innerHTML = json.message;
-                _status.style.display = 'block';
-                _save.disabled = null;
-                _save.innerHTML = 'Push';
-            } else {
-                console.log(json);
-                _putRecent(body.url, body.title, body.tags, json.id);
-                _url.value = '';
-                _title.value = '';
-                _tags.value = '';
-                _save.disabled = null;
-                _save.innerHTML = 'Push';
-            }
-        }).catch(err => console.error(err));
+        })
+            .then(resp => resp.json())
+            .then(json => {
+                if ('message' in json) {
+                    _statusMsg.innerHTML = json.message;
+                    _status.style.display = 'block';
+                    _save.disabled = null;
+                    _save.innerHTML = 'Push';
+                } else {
+                    console.log(json);
+                    _putRecent(body.url, body.title, body.tags, json.id);
+                    _url.value = '';
+                    _title.value = '';
+                    _tags.value = '';
+                    _save.disabled = null;
+                    _save.innerHTML = 'Push';
+                }
+            },err => console.error(err));
     };
 
     console.info('set hooks');
     _url.onkeyup = _loadinfo;
     _url.onmouseout = _loadinfo;
     _url.onchange = _loadinfo;
-    //_push.onclick = _pushNewItem;
 
     var buttons = document.querySelectorAll('.delete-btn');
+    // DOM elements dont have [].forEach()
     Array.prototype.forEach.call(buttons, element => {
         element.onclick = _deleteItem;
     });
